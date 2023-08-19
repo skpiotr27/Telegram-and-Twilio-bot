2735ac12772d6ae726fa20c24c597ed1c887d2e0
p.skok.27 <p.skok.27@gmail.com>
2023-08-19 10:01:13 +0200

    Add TelegramClient saving

--- log.py
+++ log.py
@@ -1,7 +1,9 @@
 from telethon.sync import TelegramClient
 
+# Set your API data
 api_id =  "YOUR_API_ID"
 api_hash = "YOUR_API_HASH"
 
+# Generate anon.session and save it
 with TelegramClient('anon', api_id, api_hash) as client:
         client.start()
